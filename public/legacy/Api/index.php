aacce55a6ecee6806440172f132e997ca388778f
ingedev94 <c.a.m.l.123@hotmail.com>
2026-05-26 18:02:52 -0500

    ajuste api y estructura

--- public/legacy/Api/index.php
+++ public/legacy/Api/index.php
@@ -1,5 +1,10 @@
 <?php
 
+// Silenciador de advertencias y obsolescencias de ConectaHogar para proteger el output JSON de la API
+error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING & ~E_DEPRECATED & ~E_STRICT);
+ini_set('display_errors', '0');
+ini_set('log_errors', '1');
+
 global $legacyRoute;
 
 if (isset($legacyRoute['script-name'], $legacyRoute['request-uri'])) {
